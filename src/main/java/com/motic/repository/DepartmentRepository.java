cb32e515e8aa7fe21345ac010c4e8fe934815b86
guotao <guotao.chen@outlook.com>
2018-11-21 21:38:08 +0800

    Initial application generated by JHipster-5.7.0

---
+++ src/main/java/com/motic/repository/DepartmentRepository.java
@@ -0,0 +1,15 @@
+package com.motic.repository;
+
+import com.motic.domain.Department;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+
+/**
+ * Spring Data  repository for the Department entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+}
